cfa9e63ab1203db98c2e945c1eea26b276623d22
akix16 <aksharax16@gmail.com>
2025-02-28 12:10:58 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-# File-no1-
-This repository is for practicing the GitHub Flow
+# This is a simple "Hello, World!" script in Python
+
+def main():
+  """Prints a greeting to the console."""
+  print("Hello, World! This is my first GitHub upload.")
+
+if __name__ == "__main__":
+  main()
